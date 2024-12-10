bfa64139ef303830e6cab8e7a02e3ab39c123a0a
Jatin-1111 <off.jatin1111@gmail.com>
2024-12-10 23:04:31 +0530

    first commit

---
+++ src/Content/page.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+function page() {
+
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default page
