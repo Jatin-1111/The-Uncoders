549e6f546637d735a52f1df1dd44b981aac98661
Jatin-1111 <off.jatin1111@gmail.com>
2024-12-21 03:03:13 +0530

    minor fixes

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyDhdoD_6FQ9EiGXUC0YpxiDRh-vrRcS-R8",
+    authDomain: "the-uncoders-vercel.firebaseapp.com",
+    projectId: "the-uncoders-vercel",
+    storageBucket: "the-uncoders-vercel.firebasestorage.app",
+    messagingSenderId: "155504288976",
+    appId: "1:155504288976:web:04725bd0b4311af0e7ead5",
+    measurementId: "G-S7QP96ZMC1",
 };
 
 const app = initializeApp(firebaseConfig);
